9939f5cb4e2d714aec50b5f876927013cb320c11
TChandanaReddy <tchandana.reddy1997@gmail.com>
2023-04-16 13:17:06 +0530

    query selector

--- dom.js
+++ dom.js
@@ -35,10 +35,11 @@
 //items[1].textContent='Hello2';
 //items[1].style.fontWeight='bold';
 //items[1].style.backgroundColor='yellow';
-var items=document.getElementsByClassName('title');
-console.log(items);
-items[0].style.fontWeight='bold';
-items[0].style.color='green';
+
+// var items=document.getElementsByClassName('title');
+// console.log(items);
+// items[0].style.fontWeight='bold';
+// items[0].style.color='green';
 
 //gives error --->  items.style.backgroundColor='#f4f4f4';
 
@@ -47,37 +48,40 @@ items[0].style.color='green';
 //}
 
 // GETELEMENTBYTAGNAME //
-//var li=document.getElementsByTagName('li');
-//console.log(li);
-//console.log(li[1]);
-//li[1].textContent='Hello2';
-//li[1].style.fontWeight='bold';
-//li[1].style.backgroundColor='yellow';
+// var li=document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent='Hello2';
+// li[1].style.fontWeight='bold';
+// li[1].style.backgroundColor='yellow';
 
 //gives error --->  items.style.backgroundColor='#f4f4f4';
 
-//for(var i=0; i<li.length;i++){
+// for(var i=0; i<li.length;i++){
 //    li[i].style.backgroundColor='#f4f4f4';
-//}
+// }
 
 // QUERYSELECTOR //
-// var header = document.querySelector('#main-header');
-// header.style.borderBottom='solid 4px #ccc';
+var header = document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
 
-// var input=document.querySelector('input');
-// input.value='Hello World';
+var input=document.querySelector('input');
+input.value='Hello World';
 
-// var submit=document.querySelector('input[type="submit"]');
-// submit.value="SEND";
+var submit=document.querySelector('input[type="submit"]');
+submit.value="SEND";
 
-// var item=document.querySelector('.list-group-item');
-// item.style.color='red';
+var item=document.querySelector('.list-group-item');
+item.style.color='red';
 
-// var lastItem=document.querySelector('.list-group-item:last-child');
-// lastItem.style.color='blue';
+var lastItem=document.querySelector('.list-group-item:last-child');
+lastItem.style.color='blue';
 
-// var secondItem=document.querySelector('.list-group-item:nth-child(2)');
-// secondItem.style.color='orange';
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green';
+
+var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility='hidden';
 
 // QUERYSELECTORALL //
 // var titles=document.querySelectorAll('.title');
